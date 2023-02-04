fd2b291c2fcd8ff7dfd1c81e47a3c2441c8d4b02
surendrakumar878 <surendramarshkole78@gmail.com>
2023-02-04 17:00:22 +0530

    first commit

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { Box, Flex, Grid, GridItem, Heading, Image, Text } from "@chakra-ui/react";
-import { PhoneIcon,AddressIcon } from "@chakra-ui/icons";
+import { PhoneIcon} from "@chakra-ui/icons";
 
 
 const Footer = () => (
   <Box w="100%" as="footer" bg="gray.700" py={8}>
     <Box pl="20px">
-      <Heading color={"white"} mb="20px">pborbit</Heading>
+<img src="./logo.png" alt="pb" />
       <Grid w="100%" templateColumns="repeat(3, 1fr)" gap={1}>
         <GridItem w="35%" color={"white"}>
           <Text>
